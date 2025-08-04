cc48c13cc82c6e8b5775bf2889c96b42d4062a6e
CadetFrancis <francis.belmes@gcash.com>
2025-08-04 10:15:58 +0800

    finished part 1,req 1-16

--- src/main/java/org/example/InteractiveOrderProcessor.java
+++ src/main/java/org/example/InteractiveOrderProcessor.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class InteractiveOrderProcessor {
@@ -12,8 +14,12 @@ public class InteractiveOrderProcessor {
 
     public static void main(String[] args) {
         //PART 1
-        System.out.println("Welcome to the Interactive Order Processor!");
+        InteractiveOrderProcessor newOrder = new InteractiveOrderProcessor();
+        TierDiscount td = newOrder.tierBasedDiscount();
+        double subtotal = newOrder.initialSubtotal();
 
+
+        System.out.println("Welcome to the Interactive Order Processor!\n");
         System.out.println("--- Enter Order Details ---");
         System.out.println("Enter unit price: ");
         unitPrice = sc.nextDouble();
@@ -38,9 +44,9 @@ public class InteractiveOrderProcessor {
         System.out.println("Discount Code: " + discountCode);
 
         System.out.println("--- Calculation Steps ---");
-        System.out.println("Initial Subtotal: ");
-        System.out.println("After Tier Discount (Gold - 15%): ");
-        System.out.println("After Quantity Discount (5% for >=5 items): ");
+        System.out.println("Initial Subtotal: " + newOrder.initialSubtotal());
+        System.out.println("After Tier Discount "+ td.description +": " + td.rate);
+        System.out.println("After Quantity Discount "+  + ": ");
         System.out.println("After Promotional Code (SAVE10 for >$75): ");
         System.out.println("After Small Order Surcharge (if applicable): ");
 
@@ -48,7 +54,32 @@ public class InteractiveOrderProcessor {
 
         System.out.println("Final Order Total: ");
 
+        sc.close();
+    }
 
+
+
+
+
+    public double initialSubtotal(){
+        return unitPrice * quantity;
+    }
+
+    public record TierDiscount(String description, double rate) {}
+
+    public TierDiscount tierBasedDiscount() {
+        if (customerTier.equalsIgnoreCase("gold")) {
+            return new TierDiscount("Gold - 15%", 0.15);
+        } else if (customerTier.equalsIgnoreCase("silver")) {
+            return new TierDiscount("Silver - 10%", 0.10);
+        } else {
+            return new TierDiscount("Regular - 0%", 0.0);
+        }
+    }
+
+
+    public void quantityDiscount(){
 
     }
+
 }
